7e51e4a00baca3b18caa4d63f56f4496afccddb2
morshedi <morshedimohamed2@gmail.com>
2023-10-29 22:07:53 +0200

    last

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,5 +1,6 @@
 const Post = require("../model/post.model");
 const ApiError = require("../utils/apiError");
+const asyncHandler = require("express-async-handler");
 
 exports.getPosts = async (req, res, next) => {
   const posts = await Post.find();
@@ -9,24 +10,24 @@ exports.getPosts = async (req, res, next) => {
   });
 };
 
-exports.createPost = async (req, res, next) => {
+exports.createPost = asyncHandler(async (req, res, next) => {
   const post = await Post.create(req.body);
   if (!post) {
     return next(new ApiError(404, "Post not found"));
   }
   return res.status(201).json({ success: true, data: post });
-};
+});
 
-exports.getPost = async (req, res, next) => {
+exports.getPost = asyncHandler(async (req, res, next) => {
   const { postId } = req.params;
   const post = await Post.findById(postId);
   if (!post) {
     return next(new ApiError(404, "Invalid Id"));
   }
   return res.status(200).json({ success: true, data: post });
-};
+});
 
-exports.updatePost = async (req, res, next) => {
+exports.updatePost = asyncHandler(async (req, res, next) => {
   const post = await Post.findByIdAndUpdate(req.params.postId, req.body, {
     new: true,
   });
@@ -34,17 +35,17 @@ exports.updatePost = async (req, res, next) => {
     return next(new ApiError(404, "Can not find this id"));
   }
   return res.status(200).json({ status: true, data: post });
-};
+});
 
-exports.deletePost = async (req, res, next) => {
+exports.deletePost = asyncHandler(async (req, res, next) => {
   const { postId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(postId))
     return next(new ApiError(404, "No post with this Id"));
   const deletedPost = await Post.findByIdAndDelete(postId);
   return res.status(204, { msg: "deleted Successful" });
-};
+});
 
-exports.likePost = async (req, res, next) => {
+exports.likePost = asyncHandler(async (req, res, next) => {
   const post = await Post.findById(req.params.postId);
   if (!post) {
     return next(new ApiError(404, "Can not find this id"));
@@ -52,4 +53,4 @@ exports.likePost = async (req, res, next) => {
   post.likeCount = post.likeCount + 1;
   await post.save();
   return res.status(200).json({ status: true, data: post });
-};
+});
